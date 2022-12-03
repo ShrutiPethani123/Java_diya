474bdd8d7b48fae73301194defe05e4801ec2245
ShrutiPethani123 <shrutipethani.royal@gmail.com>
2022-12-03 12:05:14 +0530

    init

---
+++ C.java
@@ -0,0 +1,29 @@
+class A{
+
+	public static void main(String args[])
+	{
+			System.out.println("Class A...");
+	}
+
+	
+}
+
+class B{
+	
+	public static void main(String [] b)
+	{
+			System.out.println("Class B...");
+	}
+
+}
+
+public class C
+{
+
+	public static void main(String []diya)
+	{
+			System.out.println("Class C...");
+	}
+
+
+}
